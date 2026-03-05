cb6ae4f8088b587b444d408456e955ab815d20ec
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-03-05 19:58:45 +0000

    started cpp file

---
+++ src/any/any_game_name.cpp
@@ -0,0 +1,8 @@
+#include "any_game_name.h"
+
+namespace any {
+    any_game_name::any_game_name([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
+        mj::game("any"),
+        
+
+}
