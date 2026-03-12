663d4936c976ea952eacbf939629a137d550d0c3
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-03-12 01:22:02 +0000

    updated gravity

--- src/mar/mar_player.cpp
+++ src/mar/mar_player.cpp
@@ -15,10 +15,21 @@ namespace mar
      * @param starting_position the location to start the mar_player at
      * @param speed the pixels/frame the mar_player moves at in each dimension
      */
-    mar_player::mar_player(bn::fixed_point starting_position, bn::fixed speed) : _sprite(bn::sprite_items::square.create_sprite(starting_position)), _speed(speed),
-                                                                                 _rect(bn::rect(
-                                                                                     starting_position.x().round_integer(),
-                                                                                     starting_position.y().round_integer(), 8, 8))
+    mar_player::mar_player(
+        bn::fixed_point starting_position, 
+        bn::fixed speed
+    ) :        
+        _sprite(
+            bn::sprite_items::square.create_sprite(
+                starting_position
+            )
+        ), 
+        _speed(speed),
+        _rect(
+            bn::rect(
+                starting_position.x().round_integer(),
+                starting_position.y().round_integer(), 8, 8)
+            )
     {
     }
 
@@ -32,11 +43,11 @@ namespace mar
         {
             _sprite.set_y(_sprite.y() - _speed);
         }
-
         else if (_sprite.y() < MAX_Y)
         {
-            _sprite.set_y(_sprite.y() + _speed / 1.5);
+            _sprite.set_y(_sprite.y() + _speed/4);
         }
+
         if (bn::keypad::down_held() && _sprite.y() < MAX_Y)
         {
             _sprite.set_y(_sprite.y() + _speed);
